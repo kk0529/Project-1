6f04d08130a049259ef8d7653bf5239fff22a9cf
Nicholas A Ciccarino <nciccarino@gmail.com>
2017-06-13 14:36:03 -0400

    done

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,23 +1,40 @@
- var KEY = "hdClS0YzlOmshlFFeW3W3WV1PoXEp17UCdrjsnAeTCrRrnazfX";
-
-  var queryURL = "https://webcamstravel.p.mashape.com/webcams/list";
-
-
- $.ajax({
-    url: queryURL,
-    method: 'GET',
-    headers: {
-        "X-Mashape-Key" : KEY
-    }
-  }).done(function(response) {
-    console.log(response);
-    var apiLink = queryURL + "&api_key=" + KEY;
-         console.log(apiLink);
-      var feedId = response.result.webcams[0].id;
-      console.log(feedId);
-
- 
-          var webcamDiv = $("<div>");
-          webcamDiv.attr("class", "webcamDiv");
-          $(webcamDiv).append("<a name='lkr-timelapse-player' data-id='1000550952' data-play='day' href='https://lookr.com/1000550952' target='_blank'>Beinwil am See: Hallwilersee Nord</a>");
+$(document).ready(function(){
+      console.log("ready");
+      // $(".error").html("");
+      var inputLoc = "Basking Ridge, New Jersey"; 
+      // This is our API key
+      var APIKey = "166a433c57516f51dfab1f7edaed8413";
+              
+        $("#add-weatherLocation-btn").on("click", function(event) {
+          event.preventDefault();
+          // alert("clicked");
+          inputLoc = $("#weatherLoc-input").val().trim();
+          if(inputLoc ==""){
+              $(".city").html(" ==> No Location Entered");
+            return false;
+          }
+      var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+      "q=" + inputLoc + "&units=imperial&appid=" + APIKey;
+      // Here we run our AJAX call to the OpenWeatherMap API
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      // We store all of the retrieved data inside of an object called "response"
+      .done(function(response) {
+        // Log the queryURL
+        console.log(queryURL);
+        // Log the resulting object
+        console.log(response);
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").html("Wind Speed: " + response.wind.speed);
+        $(".humidity").html("Humidity: " + response.main.humidity);
+        $(".temp").html("Temperature (F) " + response.main.temp);
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
       });
+    });
+    });
